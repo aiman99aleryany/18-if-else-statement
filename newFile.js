43b0d7a6f1813f17ae89abfa39eeffc217797613
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-17 02:30:59 +0300

    init commit

---
+++ newFile.js
@@ -0,0 +1,16 @@
+// Rewrite the code below, by using the ternary operator
+
+/*
+  let number = 7;
+  if (number > 0) {
+    console.log("The number is positive");
+  } else console.log("The number is negative");
+*/
+
+let number = 0;
+
+console.log(
+  number > 0
+    ? console.log("The number is positive")
+    : console.log("The number is negative")
+);
